f266369f60006e1f6eb018fdc9a3c302b4207d03
BeHustle <46607810+BeHustle@users.noreply.github.com>
2020-06-14 19:03:26 +0300

    Добавляет проверку данных через prop-types

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Main from '../main/main.jsx';
+import PropTypes from 'prop-types';
+import {CARD_TYPES, CARD_NAMES} from '../../constants';
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {countOffers, userEmail} = props;
+  const {countOffers, userEmail, cardPrice, cardName, cardType} = props;
   return <Main
-    countOffers={countOffers}
-    userEmail={userEmail}
+    countOffers = {countOffers}
+    userEmail = {userEmail}
+    cardPrice = {cardPrice}
+    cardName = {cardName}
+    cardType = {cardType}
   />;
+};
+
+App.propTypes = {
+  countOffers: PropTypes.number.isRequired,
+  userEmail: PropTypes.string.isRequired,
+  cardPrice: PropTypes.number.isRequired,
+  cardName: PropTypes.oneOf(CARD_NAMES),
+  cardType: PropTypes.oneOf(CARD_TYPES)
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import App from './components/app/app.jsx';
 
 ReactDOM.render(
     <App
-      countOffers={`128`}
+      countOffers={128}
       userEmail={`Oliver.conner@gmail.com`}
+      cardPrice = {256}
+      cardName = {`Beautiful & luxurious apartment at great location`}
+      cardType = {`Appartment`}
     />,
     document.querySelector(`#root`)
 );

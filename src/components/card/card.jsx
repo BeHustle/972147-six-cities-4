7a093f8810173f9a559a37315c9dd60a84873913
BeHustle <46607810+BeHustle@users.noreply.github.com>
2020-06-15 16:01:09 +0300

    Использует короткий синтаксис возврата в стрелочных функциях

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,44 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {CARD_TYPES, CARD_NAMES} from '../../constants';
 
-const Card = ({price, name, type}) => {
-  return (
-    <article className="cities__place-card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
-      <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
-        </a>
-      </div>
-      <div className="place-card__info">
-        <div className="place-card__price-wrapper">
-          <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{price}</b>
-            <span className="place-card__price-text">&#47;&nbsp;night</span>
-          </div>
-          <button className="place-card__bookmark-button button" type="button">
-            <svg className="place-card__bookmark-icon" width="18" height="19">
-              <use xlinkHref="#icon-bookmark"/>
-            </svg>
-            <span className="visually-hidden">To bookmarks</span>
-          </button>
+const Card = ({price, name, type}) =>
+  <article className="cities__place-card place-card">
+    <div className="place-card__mark">
+      <span>Premium</span>
+    </div>
+    <div className="cities__image-wrapper place-card__image-wrapper">
+      <a href="#">
+        <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+      </a>
+    </div>
+    <div className="place-card__info">
+      <div className="place-card__price-wrapper">
+        <div className="place-card__price">
+          <b className="place-card__price-value">&euro;{price}</b>
+          <span className="place-card__price-text">&#47;&nbsp;night</span>
         </div>
-        <div className="place-card__rating rating">
-          <div className="place-card__stars rating__stars">
-            <span style={{width: `80%`}}/>
-            <span className="visually-hidden">Rating</span>
-          </div>
-        </div>
-        <h2 className="place-card__name">
-          <a href="#">{name}</a>
-        </h2>
-        <p className="place-card__type">{type}</p>
+        <button className="place-card__bookmark-button button" type="button">
+          <svg className="place-card__bookmark-icon" width="18" height="19">
+            <use xlinkHref="#icon-bookmark"/>
+          </svg>
+          <span className="visually-hidden">To bookmarks</span>
+        </button>
       </div>
-    </article>
-  );
-};
+      <div className="place-card__rating rating">
+        <div className="place-card__stars rating__stars">
+          <span style={{width: `80%`}}/>
+          <span className="visually-hidden">Rating</span>
+        </div>
+      </div>
+      <h2 className="place-card__name">
+        <a href="#">{name}</a>
+      </h2>
+      <p className="place-card__type">{type}</p>
+    </div>
+  </article>;
 
 Card.propTypes = {
   price: PropTypes.number.isRequired,

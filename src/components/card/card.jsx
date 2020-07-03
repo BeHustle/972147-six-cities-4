d4a6ee4beebd04c6522e2d1a856ec16480c48a3d
BeHustle <46607810+BeHustle@users.noreply.github.com>
2020-07-04 00:27:46 +0300

    Проводит рефакторинг

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,50 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {CARD_TYPES} from '../../constants';
 
-const Card = (props) => {
-  const {offer, onTitleClick, onCardHover} = props;
-  const {id, name, price, image, type, isPremium, inBookmarks, rating} = offer;
-  const bookmarksBtnClass = inBookmarks ? `place-card__bookmark-button button` : `place-card__bookmark-button place-card__bookmark-button--active button`;
-  const ratingWidth = `${rating * 20}%`;
-  return (<article
-    className="cities__place-card place-card"
-    onMouseEnter={() => onCardHover(id)}
-    onMouseLeave={() => onCardHover(null)}>
+const IN_BOOKMARKS_CLASS = `place-card__bookmark-button--active`;
 
-    {isPremium && <div className="place-card__mark">
-      <span>Premium</span>
-    </div>}
-    <div className="cities__image-wrapper place-card__image-wrapper">
-      <a href="#">
-        <img className="place-card__image" src={image} width="260" height="200" alt="Place image"/>
-      </a>
-    </div>
-    <div className="place-card__info">
-      <div className="place-card__price-wrapper">
-        <div className="place-card__price">
-          <b className="place-card__price-value">&euro;{price}</b>
-          <span className="place-card__price-text">&#47;&nbsp;night</span>
-        </div>
-        <button className={bookmarksBtnClass} type="button">
-          <svg className="place-card__bookmark-icon" width="18" height="19">
-            <use xlinkHref="#icon-bookmark"/>
-          </svg>
-          <span className="visually-hidden">To bookmarks</span>
-        </button>
+const Card = ({onTitleClick, onCardHover, offer: {
+  id, name, price, image, type, isPremium, inBookmarks, rating
+}}) => <article
+  className="cities__place-card place-card"
+  onMouseEnter={() => onCardHover(id)}
+  onMouseLeave={() => onCardHover(null)}>
+
+  {isPremium && <div className="place-card__mark">
+    <span>Premium</span>
+  </div>}
+  <div className="cities__image-wrapper place-card__image-wrapper">
+    <a href="#">
+      <img className="place-card__image" src={image} width="260" height="200" alt="Place image"/>
+    </a>
+  </div>
+  <div className="place-card__info">
+    <div className="place-card__price-wrapper">
+      <div className="place-card__price">
+        <b className="place-card__price-value">&euro;{price}</b>
+        <span className="place-card__price-text">&#47;&nbsp;night</span>
       </div>
-      <div className="place-card__rating rating">
-        <div className="place-card__stars rating__stars">
-          <span style={{width: ratingWidth}}/>
-          <span className="visually-hidden">Rating</span>
-        </div>
-      </div>
-      <h2 className="place-card__name">
-        <a onClick={onTitleClick} href="#">{name}</a>
-      </h2>
-      <p className="place-card__type">{type}</p>
+      <button className={`place-card__bookmark-button button ${inBookmarks && IN_BOOKMARKS_CLASS}`} type="button">
+        <svg className="place-card__bookmark-icon" width="18" height="19">
+          <use xlinkHref="#icon-bookmark"/>
+        </svg>
+        <span className="visually-hidden">To bookmarks</span>
+      </button>
     </div>
-  </article>);
-};
+    <div className="place-card__rating rating">
+      <div className="place-card__stars rating__stars">
+        <span style={{width: `${Math.ceil(rating) * 20}%`}}/>
+        <span className="visually-hidden">Rating</span>
+      </div>
+    </div>
+    <h2 className="place-card__name">
+      <a onClick={onTitleClick} href="#">{name}</a>
+    </h2>
+    <p className="place-card__type">{type}</p>
+  </div>
+</article>;
 
 Card.propTypes = {
   onTitleClick: PropTypes.func.isRequired,

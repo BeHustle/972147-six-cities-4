501564ab8525e2f4e1ed793c54d49237910e2136
BeHustle <46607810+BeHustle@users.noreply.github.com>
2020-07-03 23:50:19 +0300

    Добавляет react router в app. WIP

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Main from '../main/main.jsx';
+import CardDetail from '../card-detail/card-detail.jsx';
 import {offers} from '../../mocks/offers.js';
 import {email} from '../../mocks/user.js';
 
 const App = () =>
-  <Main
-    countOffers={128}
-    userEmail={email}
-    onCardTitleClick={() => {}}
-    offers={offers}
-  />;
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Main
+          countOffers={128}
+          userEmail={email}
+          onCardTitleClick={() => {}}
+          offers={offers}
+        />
+      </Route>
+      <Route exact path="/offer">
+        <CardDetail
+          userEmail={email}
+          offer={offers[0]}
+        />
+      </Route>
+    </Switch>
+  </BrowserRouter>;
 
 export default App;

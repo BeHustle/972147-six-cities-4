b0fd446a2d275bcac715fcaf1c40c9f02718850d
BeHustle <46607810+BeHustle@users.noreply.github.com>
2020-07-18 20:31:39 +0300

    Добавляет функционал изменения сортировки и выбора активной метки в reducer

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,26 +1,32 @@
 import {offers} from '../mocks/offers.js';
 import {cities} from '../mocks/cities.js';
 import {email} from '../mocks/user.js';
-
+import {Sorts} from '../constants.js';
 
 const initialState = {
   city: cities[0],
   offers,
   cities,
-  userEmail: email
+  userEmail: email,
+  sorts: Sorts,
+  activeSort: Sorts.POPULAR,
+  activeOfferId: null
 };
 
 const ActionTypes = {
   CHANGE_CITY: `CHANGE_CITY`,
-  CHANGE_OFFERS: `CHANGE_OFFERS`
+  CHANGE_SORT: `CHANGE_SORT`,
+  CHANGE_ACTIVE_OFFER_ID: `CHANGE_ACTIVE_OFFER_ID`
 };
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case ActionTypes.CHANGE_CITY:
       return Object.assign({}, state, {city: action.payload});
-    case ActionTypes.CHANGE_OFFERS:
-      return Object.assign({}, state, {offers: action.payload});
+    case ActionTypes.CHANGE_SORT:
+      return Object.assign({}, state, {activeSort: action.payload});
+    case ActionTypes.CHANGE_ACTIVE_OFFER_ID:
+      return Object.assign({}, state, {activeOfferId: action.payload});
     default:
       return state;
   }
